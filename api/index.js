f8da6a60c541ca16b3df7efbbace84d56318bbf9
InderKumhar0 <inderkumhar58@gmail.com>
2023-12-16 22:38:34 +0530

    first commit

--- api/index.js
+++ api/index.js
@@ -1,25 +1,30 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
-import cookieParser from 'cookie-parser';
-import path from 'path';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
-mongoose
-  .connect(process.env.DB).then(()=>{
-    console.log("Connected to DB.")
-  });
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.DB).then(() => {
+      console.log("Connected to DB.");
+    });
+  } catch (error) {
+    throw error;
+  }
+};
 
 const __dirname = path.resolve();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 app.use(express.json());
@@ -27,15 +32,16 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+  connect();
+  console.log("Server listening on port 3000");
 });
 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     message,
