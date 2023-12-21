051ef9275b9ba540c55fee64f9f6a929de32ee08
InderKumhar0 <inderkumhar58@gmail.com>
2023-12-21 12:17:02 +0530

    retrieve posts

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import {createPost, getPost} from '../controllers/post.controller.js';
+import express from "express";
+import { createPost, getPost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
-router.route('/').post(createPost).get(getPost);
+router.post("/", createPost);
+router.post("/:id", getPost);
 
 export default router;
